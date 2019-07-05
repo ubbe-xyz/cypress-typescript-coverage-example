22d00fc46f05da5ae26746838f013a27c4bc54de
Lluis Agusti <lluis.agusti@zopa.com>
2019-07-05 16:05:37 +0200

    Setup from `create-react-app`

---
+++ README.md
@@ -0,0 +1,3 @@
+# Cypress code coverage of Typescript app 💃🏻
+
+This repository illustratates how to setup code coverage in Cypress of a Typescript app.
---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Hello world ...{' '}
+          <span role="img" aria-label="planet earth">
+            🌏
+          </span>
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
