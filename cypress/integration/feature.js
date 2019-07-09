1dfbe4620be8db021dc5480df241b3ba1995c2c7
Lluis Agusti <lluis.agusti@zopa.com>
2019-07-09 13:16:57 +0200

    Test greeting the user

--- cypress/integration/feature.js
+++ cypress/integration/feature.js
@@ -1,3 +1,9 @@
-it('works', () => {
-  expect(true).to.be.true;
+describe('When landing on the application', () => {
+  before(() => {
+    cy.visit('/');
+  });
+
+  it('should greet the user', () => {
+    cy.getByText('Hello world ...');
+  });
 });
